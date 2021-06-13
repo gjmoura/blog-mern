21bfd52e514f0c31f9bba94ff393fde71f9e3903
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2021-06-13 18:16:43 -0300

    :art: Atualiza a navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,25 +1,37 @@
 import React from "react"
 import {Navbar, Nav, Image} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
-import Logo from '../../assets/images/Logo.png'
+import Logo from '../../assets/images/Brand2.svg'
 import Profile from '../../assets/images/Profile.png'
+import { FaRegListAlt, FaRegUserCircle, FaPenSquare, FaBars} from 'react-icons/fa'
 import "./style.scss"
 
 const Navigationbar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar>
       <Navbar.Brand>
         <Link to="/">
           Blog's
         </Link>
       </Navbar.Brand>
         <Nav className="mr-auto">
-          <Link to="/ViewFeed">Feed</Link>
-          <Link to="/ViewRegisterPost">Postar</Link>
-          <Link to="/ViewProfile">Perfil</Link>
+          <Link to="/ViewFeed" className="mr-3 d-flex">
+            <FaRegListAlt className="mr-1"/>
+            Feed
+          </Link>
+          <Link to="/ViewRegisterPost">
+            <FaPenSquare className="mr-1"/>
+            Postar
+          </Link>
+
+          <Link to="/ViewProfile" className="ml-3">
+            <FaRegUserCircle className="mr-1"/>
+            Perfil
+          </Link>
         </Nav>
         <Nav>
-          <Image src={Profile} height="30" width="30" roundedCircle />
+          <Image src={Profile} height="30" width="30" roundedCircle className="ml-3"/>
+          <FaBars color="white" className="ml-3"/>
         </Nav>
     </Navbar>
   )
